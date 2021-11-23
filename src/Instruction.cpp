a20a07a225d5aceb94f1f8e8fc79530853b5c7c6
Tetsuya-Nishikawa <te1823ya@gmail.com>
2021-11-23 17:36:10 +0900

    modify Sei::CompileStep

--- src/Instruction.cpp
+++ src/Instruction.cpp
@@ -1,6 +1,32 @@
 #include "Instruction.hpp"
 #include "Cpu.hpp"
 
+//cpuの状態を実機レジスタに対応させる。
+//acc:al
+//sp:ah
+//x:bl
+//y:bh
+//status:cl
+//pc:di
+//MOV AL, Imm8
+//空いてるレジスタはEDX, ESI
+
+/***
+        union{
+            uint8_t raw;
+            struct{
+                unsigned C:1;
+                unsigned Z:1;
+                unsigned I:1;
+                unsigned D:1;
+                unsigned B:1;
+                unsigned R:1;
+                unsigned V:1;
+                unsigned N:1;
+            }flgs;
+        }P;
+***/
+
 InstructionBase::InstructionBase(string name, int nbytes, int cycles){
     this->name = name;
     this->nbytes = nbytes;
@@ -21,5 +47,16 @@ int Sei::Execute(Cpu* cpu){
 }
 
 int Sei::CompileStep(uint8_t** code, bool* stop){
-    this->Error("Not implemented: %s::Run", this->name.c_str());
+    *stop = false;
+    if(*code!=NULL){
+        //status:cl
+        //OR CL, 0x04
+        **code = 0x80;
+        *code++;
+        **code = 0xC9;
+        *code++;
+        **code = (uint8_t)0x04;
+        *code++;
+    }
+    return 3;
 }
--- src/Jit.cpp
+++ src/Jit.cpp
@@ -3,6 +3,16 @@
 #include "Bus.hpp"
 #include "Instruction.hpp"
 
+//cpuの状態を実機レジスタに対応させる。
+//acc:al
+//sp:ah
+//x:bl
+//y:bh
+//status:cl
+//pc:di
+//MOV AL, Imm8
+//空いてるレジスタはEDX, ESI
+
 Jit::Jit(Cpu* cpu, Bus* bus){
     this->cpu = cpu;
     this->bus = bus;
@@ -24,19 +34,21 @@ bool Jit::IsCompiledBlock(uint16_t pc){
 }
 
 uint8_t* Jit::CompileBlock(uint16_t pc){
-    uint8_t* code;
-    bool stop;
-    uint8_t op_code = this->bus->Read8(pc);
-    //機械語命令を1つコンパイル
-    if(this->instructions[op_code]==NULL){
-        this->Error("Not implemented: %02X at Jit::CompileBlock", op_code);
+    uint8_t* code = NULL;
+    bool stop = false;
+    while(!stop){
+        uint8_t op_code = this->bus->Read8(pc);
+        pc++;
+        if(this->instructions[op_code]==NULL){
+            this->Error("Not implemented: 0x%02X at Jit::CompileBlock", op_code);
+        }
+        this->instructions[op_code]->CompileStep(&code, &stop);
     }
-    this->instructions[op_code]->CompileStep(&code, &stop);
     return NULL;
 }
 
 void Jit::Run(uint16_t pc){
-    uint8_t* code;
+    uint8_t* code = NULL;
     if(this->IsCompiledBlock(pc)){
         code = this->pc2code[pc];
     }else{
